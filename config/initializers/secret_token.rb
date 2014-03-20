ceaf1991003a74d05366a602612e821afc81523f
FLASh <User@flash.local>
2014-03-20 20:10:31 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripwithme::Application.config.secret_key_base = '2be4d7f920b39d6dffeb6375e3c8cd09af6c6f1ed11d494a8cecfb299b5badf689577b6b001dd468d064add018dc0a9dea0f3596e71c4308d82f49810ce11faa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tripwithme::Application.config.session_store :cookie_store, key: '_tripwithme_session'
