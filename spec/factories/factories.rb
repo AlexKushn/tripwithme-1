eb8affda858847d2cb5c58eff55b9325706ecd83
Garrett <garrett@4540s.(none)>
2014-03-30 18:18:22 +0300

    factories

---
+++ spec/factories/factories.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :trip do
+    start { Faker::Address.city }
+    stop  { Faker::Address.city }
+    start_time { Date.today + rand(20) }
+    price { rand(10) }
+    passengers { rand(5) }
+    description { Faker::Lorem.paragraph }
+  end
+
+  factory :comment do
+    sequence(:title) { |n| "Title_ #{n}" }
+    text { Faker::Lorem.paragraph }
+    trip
+  end
+end
