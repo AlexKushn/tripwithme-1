cc27e1daafb7225783e824ff6ac74ddb339c9f4a
alex <alexmi-6@mail.ru>
2014-04-14 19:30:42 +0300

    check 'validates'  in  models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,9 +3,9 @@ class Trip < ActiveRecord::Base
   has_and_belongs_to_many :users
   validates :start, :stop, :start_time, presence: true
   validates :passengers, numericality: {greater_than_or_equal_to: 1, less_than: 10, only_integer: true }
-  validates :price, numericality: { greater_than_or_equal_to: 0.00 },allow_nil: true
-  validates :description, length: { maximum: 140 }, allow_nil: true
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than: 6, only_integer: true }, allow_nil: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.00 }
+  validates :description, length: { maximum: 140 }
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than: 6, only_integer: true }
   accepts_nested_attributes_for :comments, allow_destroy: true
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many  :trips
   validates :name, length: { in: 2..15}, allow_nil: true
   validates :surname, length: { in: 2..20 }, allow_nil: true
-  validates :phone_num, uniqueness: true, allow_nil: true
+  validates :phone_num, uniqueness: true, format: { with:
+  /(?<!\w)(?:(?:(?:(?:\+?3)?8\W{0,5})?0\W{0,5})?[34569]\s?\d[^\w,;(\+]{0,5})?\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d(?!(\W?\d))/x,
+  message: " only valid nubember"}, allow_nil: true
+  validates :role, inclusion: { in: %w(Driver Passenger), message: "%{value} is not a valid"}, allow_nil: true
   accepts_nested_attributes_for :cars, allow_destroy: true
 end
